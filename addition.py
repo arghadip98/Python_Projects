4641f64be7dbc1fbe40271bd5c516d90699cedb3
arghadip98 <103139892+arghadip98@users.noreply.github.com>
2022-04-07 12:17:38 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,6 @@
+num1 = 25
+num2 = 25
+
+sum = num1 + num2
+
+print(sum)
---
+++ average.py
@@ -0,0 +1,6 @@
+x = int(input("Enter 1st number : "))
+y = int(input("Enter 2nd number : "))
+
+z = ((x + y) / 2)
+
+print ("The average of the numbers are : ", z)
